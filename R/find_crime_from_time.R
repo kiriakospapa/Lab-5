4243237fe9f65f2de868cebd9c2b076c5cd589f4
Your Name <you@example.com>
2022-09-26 22:23:54 +0800

    Should be final version of time and type

--- R/find_crime_from_time.R
+++ R/find_crime_from_time.R
@@ -1,22 +1,12 @@
 library(shiny)
 library(ggmap)
 
-ui <- fluidPage(
-  
-  numericInput(inputId = "month", 
-               label = "month", 
-               value = 1, min = 1, max = 12, step = 1),
-  
-  plotOutput(outputId = "find_crime_from_month")
-  
-)
 
-server <- function(input, output) {
-  
-  output$find_crime_from_month <- renderPlot({
-    
-    x <- input$month
-    data <- crime
+
+#####Function outside in order to test#####
+
+  find_crime_from_month<-function(data,x){
+
     if(x == 1){month <- "january"}
     else if(x == 2){month <- "february"}
     else if(x == 3){month <- "march"}
@@ -27,16 +17,32 @@ server <- function(input, output) {
     else if(x == 8){month <- "august"}
     else if(x > 8 ){stop("Only data from January to August available")}
 
+
     index_of_month <- which(data$month == month)
     data_of_the_month <- data[index_of_month,]
 
     huston <- c(left= -95.4, bottom = 29.6, right = -95, top = 29.8)
     map <-ggmap::get_stamenmap(huston, maptype = "terrain", zoom=10)
-    ggmap(map) + ggplot2::geom_jitter(data = data_of_the_month, ggplot2::aes(x=lon, y=lat),size = 0.0005, color = "red")
+    crime_map <-ggmap(map) + ggplot2::geom_jitter(data = data_of_the_month, ggplot2::aes(x=lon, y=lat),size = 0.0005, color = "red")
 
+    return(crime_map)
+  }
 
-  })
- 
-}
+  ##### End of Function #####
+  
+  
+  ##### Shiny #####
+  
+  ui <- fluidPage(
+  
+    numericInput(inputId = "month", label = "month", value = 1, min = 1, max = 12, step = 1),
+  
+    plotOutput(outputId = "find_crime_from_month")
+  
+  )
+  
+  server <- function(input, output) {
+    output$find_crime_from_month<- renderPlot(find_crime_from_month(crime,input$month))
+  }
 
 shinyApp(ui, server)
--- R/find_crime_from_type.R
+++ R/find_crime_from_type.R
@@ -8,53 +8,46 @@
 
 
 
-#####Start of use for test#####
+#####Function outside in order to test#####
 
-find_crime_from_type <- function(data,type){
+  find_crime_from_type <- function(data,type){
   
-  if(type != "theft" & 
-     type != "auto theft" & 
-     type != "murder"& 
-     type != "robbery"& 
-     type != "aggravated assault"& 
-     type != "burglary")
-    {stop("Wrong Input")}
+    if(type != "theft" & 
+       type != "auto theft" & 
+      type != "murder"& 
+       type != "robbery"& 
+      type != "aggravated assault"& 
+       type != "burglary"){stop("Wrong Input, only: theft, auto theft, murder,robbery,aggravated assault,burglary.can be searched")}
   
   
-  index_of_type <- which(data$offense == as.character(type))
-  data_of_type <- data[index_of_type,]
+   index_of_type <- which(data$offense == as.character(type))
+   data_of_type <- data[index_of_type,]
   
-  huston <- c(left= -95.4, bottom = 29.6, right = -95, top = 29.8)
-  map <- ggmap::get_stamenmap(huston, maptype = "terrain", zoom=10)
-  crime_map<- ggmap::ggmap(map) + ggplot2::geom_jitter(data = data_of_type, ggplot2::aes(x=lon, y=lat),size = 0.005, color = "red")
+   huston <- c(left= -95.4, bottom = 29.6, right = -95, top = 29.8)
+   map <- ggmap::get_stamenmap(huston, maptype = "terrain", zoom=10)
+   crime_map<- ggmap::ggmap(map) + ggplot2::geom_jitter(data = data_of_type, ggplot2::aes(x=lon, y=lat),size = 0.005, color = "red")
   
-  return(crime_map)
-}
+   return(crime_map)
+  }
 
 
-##### End of use for test #####
-
-##### Load Data #####
-crime <- ggmap::crime
+  ##### End of Function #####
 
 
-
-
-##### Shiny #####
-ui <- shiny::fluidPage(
+  ##### Shiny #####
+  ui <- shiny::fluidPage(
   
-  shiny::textInput(inputId = "crime_type",
-                   label = "crime_type" ),
+    shiny::textInput(inputId = "crime_type",label = "crime_type" ),
   
-  shiny::plotOutput(outputId = "find_crime_from_type")
+    shiny::plotOutput(outputId = "find_crime_from_type")
   
-)
+  )
 
-server <- function(input, output) {
-  output$find_crime_from_type<- renderPlot(find_crime_from_type(crime,input$crime_type))
-}
+  server <- function(input, output) {
+    output$find_crime_from_type<- renderPlot(find_crime_from_type(crime,input$crime_type))
+  }
 
-shiny::shinyApp(ui=ui, server=server)
+  shiny::shinyApp(ui=ui, server=server)
 
 
 
