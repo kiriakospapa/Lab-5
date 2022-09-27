d4a1ee218d0c01615decd9913f9ea412cac09c2f
Your Name <you@example.com>
2022-09-27 18:28:13 +0800

    Try to fix note with golbal variable but havent fix yet

--- R/lab5.R
+++ R/lab5.R
@@ -45,7 +45,7 @@ find_crime_from_time_and_type <- function(data,type,x){
   else if(x == 6){month <- "june"}
   else if(x == 7){month <- "july"}
   else if(x == 8){month <- "august"}
-  else if(x > 8 ){stop("Only data from January to August available")}
+  else if(x > 8 | x <= 0 ){stop("Month should be an integer from 1 to 8")}
   
   index_of_month <- which(data$month == month)
   data_of_the_month <- data[index_of_month,]
@@ -91,8 +91,8 @@ wind_plot <- function(min, max){
 ui <- fluidPage(
   
   selectInput( "option", label = "Select what you want to see",
-              choices = list("Winds in Huston", "Crimes in huston"),
-              selected = NULL, width = "20%"),
+               choices = list("Winds in Huston", "Crimes in huston by month and time", "Crimes in huston by month", "Crimes in huston by time"),
+               selected = NULL, width = "20%"),
   textInputRow(inputId="min_wind", label="Minimum of wind", value = 0, class="input-small"),
   textInputRow(inputId="max_wind", label="Maximum of wind", value = 100),
   plotOutput(outputId="probs")
@@ -103,44 +103,49 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   result <- ""
   observe({  result <- input$option
-            print(result)
-            output$probs <- renderPlot(
-              
-              # Change plot depending on the option selected
-              { 
-                if (input$option == "Winds in Huston"){
-                  updateActionButton(session, "min_wind",
-                                     label = "Minumum of wind")
-                  updateActionButton(session, "max_wind",
-                                     label = "Maximum of wind")
-                  
-                  # updateNumericInput(session, "min_wind", value = "0")
-                  # updateNumericInput(session, "max_wind", value = "100")
-                  
-                   if(input$min_wind == ""| input$max_wind == ""){
-                        stop("Insert a value")
-                   } 
-                  else if (is.na(as.integer(input$min_wind)) | is.na(input$max_wind)){
-                      stop("Insert integer not charachters")
-                    }
-                  else{
-                    wind_plot(as.integer(input$min_wind), as.integer(input$max_wind))
-                    }
-                }
-                else{
-                  updateActionButton(session, "min_wind",
-                                     label = "Month")
-                  # updateNumericInput(session, "min_wind", value = "1")
-                  
-                  updateActionButton(session, "max_wind",
-                                     label = "Type of crime")
-                  # updateNumericInput(session, "max_wind", value = "murder")
-                  print(paste(input$min_wind, input$max_wind))
-                  plot(find_crime_from_time_and_type(crime, input$max_wind, as.integer(input$min_wind)))
-                }
-                
-            })
-   })
+  print(result)
+  output$probs <- renderPlot(
+    
+    # Change plot depending on the option selected
+    { 
+      if (input$option == "Winds in Huston"){
+        updateActionButton(session, "min_wind",
+                           label = "Minumum of wind")
+        updateActionButton(session, "max_wind",
+                           label = "Maximum of wind")
+        
+        # updateNumericInput(session, "min_wind", value = "0")
+        # updateNumericInput(session, "max_wind", value = "100")
+        
+        if(input$min_wind == ""| input$max_wind == ""){
+          stop("Insert a value")
+        } 
+        else if (is.na(as.integer(input$min_wind)) | is.na(as.integer(input$max_wind))){
+          stop("Insert integer not charachters")
+        }
+        else{
+          wind_plot(as.integer(input$min_wind), as.integer(input$max_wind))
+        }
+      }
+      else if(input$option == "Crimes in huston by month and time"){
+        updateActionButton(session, "min_wind",
+                           label = "Month")
+        # updateNumericInput(session, "min_wind", value = "1")
+        
+        updateActionButton(session, "max_wind",
+                           label = "Type of crime")
+        
+        if(as.double(input$min_wind) %% 1 == 0)
+        {
+          # updateNumericInput(session, "max_wind", value = "murder")
+          plot(find_crime_from_time_and_type(crime, input$max_wind, as.integer(input$min_wind)))
+        }else{
+          stop("Month should be an integer from 1 to 8" )
+        }
+      }
+      
+    })
+  })
 }
 
 shinyApp(ui, server)
