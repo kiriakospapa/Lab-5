1b754158311c163866d4c808b964884ef14f1bb8
Your Name <you@example.com>
2022-09-27 23:52:51 +0800

    commented out Shiny part

--- R/lab5.R
+++ R/lab5.R
@@ -174,7 +174,7 @@ wind_plot <- function(min, max){
 #=================== DESING THE UI =======================
 
 ui <- fluidPage(
-  
+
   useShinyjs(),
   selectInput( "option", label = "Select what you want to see",
 
@@ -195,51 +195,51 @@ server <- function(input, output, session) {
 
             print(result)
             output$probs <- renderPlot(
-              
+
               # Change plot depending on the option selected
-              { 
+              {
                 if (input$option == "Winds in Huston"){
                   show("max_wind")
                   updateActionButton(session, "min_wind",
                                      label = "Minumum of wind")
                   updateActionButton(session, "max_wind",
                                      label = "Maximum of wind")
-                  
+
                   # updateNumericInput(session, "min_wind", value = "0")
                   # updateNumericInput(session, "max_wind", value = "100")
-                  
+
                    if(input$min_wind == ""| input$max_wind == ""){
-                        
+
                      stop("Insert a value")
-                   } 
+                   }
                   else if (is.na(as.integer(input$min_wind)) | is.na(as.integer(input$max_wind))){
-                      
+
                     stop("Insert integer not charachters")
                     }
                   else{
-                    
+
                     wind_plot(as.integer(input$min_wind), as.integer(input$max_wind))
                     }
                 }
                 else if(input$option == "Crimes in huston by month and type"){
-                  
+
                   show("max_wind")
                   show("min_wind")
-                  
-                  
-                  
+
+
+
                   updateActionButton(session, "min_wind",label = "Month")
 
                   updateActionButton(session, "max_wind",
                                      label = "Type of crime")
-                  
+
                   if(as.double(input$min_wind) %% 1 == 0)
                   {
-                 
+
                     plot(find_crime_from_time_and_type(crime, input$max_wind, as.integer(input$min_wind)))
                   }
                   else{
-                    
+
                     stop("Month should be an integer from 1 to 8" )
                   }
                 }
@@ -252,27 +252,27 @@ server <- function(input, output, session) {
                                      label = "Month")
                   if(as.double(input$min_wind) %% 1 == 0)
                   {
-                    
+
                     find_crime_from_month(crime, as.integer(input$min_wind))
                   }
                   else{
-                    
+
                     stop("Month should be an integer from 1 to 8" )
                   }
                 }
                 else if(input$option == "Crimes in huston by type"){
                   shinyjs::hide("min_wind")
                   shinyjs::show("max_wind")
-                  
-                  
+
+
                   updateActionButton(session, "max_wind",
                                      label = "Type of crime")
                   updateActionButton(session, "min_wind",
                                      label = "")
                   find_crime_from_type(crime,input$max_wind)
-                  
+
                 }
-                
+
             })
    })
 
